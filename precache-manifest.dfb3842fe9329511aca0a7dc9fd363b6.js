4f9a583e928dac657163c2c2452dcb301f5e8157
Aurélio Dias <aurelio.dias@gmail.com>
2018-10-30 14:34:33 -0300

    Updates

---
+++ precache-manifest.dfb3842fe9329511aca0a7dc9fd363b6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "26ead58317a649cdf406",
+    "url": "/tic-tac-toe/static/js/runtime~main.26ead583.js"
+  },
+  {
+    "revision": "3a3f37161c1fc11ab7a1",
+    "url": "/tic-tac-toe/static/js/main.3a3f3716.chunk.js"
+  },
+  {
+    "revision": "8391c07e1621390095e6",
+    "url": "/tic-tac-toe/static/js/1.8391c07e.chunk.js"
+  },
+  {
+    "revision": "3a3f37161c1fc11ab7a1",
+    "url": "/tic-tac-toe/static/css/main.ce80e377.chunk.css"
+  },
+  {
+    "revision": "20701b28be1d3818fd5c85ce1f4d0844",
+    "url": "/tic-tac-toe/index.html"
+  }
+];
